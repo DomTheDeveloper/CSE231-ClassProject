323a24695bd0522841052e43287bdc3a6bff07b8
Humam Abusitta <hbabusit@oakland.edu>
2015-04-06 17:10:52 -0400

    Update 5:10 20150604

--- production/ClassProject/Middleware/BinaryTree.java
+++ production/ClassProject/Middleware/BinaryTree.java
@@ -1,4 +1,5 @@
 package edu.oakland.production.ClassProject.middleware;
+import java.util.*;
 
 public class BinaryTree{
 	
@@ -41,12 +42,38 @@ public class BinaryTree{
 			}
 		}
 		
+		public ArrayList<Node> preorderTraverseTreeNodes(Node focusNode) {
+			ArrayList<Node> nodes = new ArrayList<Node>();
+			if(focusNode != null){
+				nodes.add(focusNode);
+				ArrayList<Node> left = preorderTraverseTreeNodes(focusNode.leftChild);
+				for(int i = 0; i < left.size(); i++)
+					nodes.add(left.get(i));
+				ArrayList<Node> right =	preorderTraverseTreeNodes(focusNode.rightChild);
+				for(int i = 0; i < right.size(); i++) {
+					nodes.add(right.get(i));
+				}
+			}
+			return nodes;
+		}
+		/* public List<Node> findNodes(int tgs){
+			Node focusNode = root;
+			
+			for (int i = focusNode.count(); i > 0; i++){
+				
+				if (child.tg < 90)
+					child.remove();
+			}
+			
+			
+		} */
+		
 		public Node findNode(int tg){
+			/* List<Node> lstNodes = new List<Node>(); */
+			
 			Node focusNode = root;
 			while(focusNode.tg != tg){
-				if(tg > focusNode.tg){
-					return focusNode;
-				}
+				
 				if(tg < focusNode.tg){
 					focusNode = focusNode.leftChild;
 				}
@@ -55,8 +82,30 @@ public class BinaryTree{
 				} 
 			}
 			return focusNode;
-			
 		}
-	
+		
+		public ArrayList<Node> getLess(int tg) {
+			return getLess(root, tg);
+		}
+		
+		private ArrayList<Node> getLess(Node n, int tg) {
+			ArrayList<Node> nodes = new ArrayList<Node>();
+			if(n.tg < tg) {
+				nodes.add(n);
+			}
+			if(n.leftChild != null) {
+				ArrayList<Node> left = getLess(n.leftChild, tg);
+				for(int i = 0 ; i < left.size(); i++){
+					nodes.add(left.get(i));
+				}
+			}
+			if(n.rightChild != null ) {
+				ArrayList<Node> right = getLess(n.rightChild,tg);
+				for(int i = 0 ; i < right.size(); i++){
+					nodes.add(right.get(i));
+				}
+			}
+			return nodes;
+		}
 	}
 
