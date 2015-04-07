7b263ae9a04b14ff295dfc8d56aa869a796d1e1c
auljaj <auljaj@users.noreply.github.com>
2015-04-07 13:09:35 -0400

    Update ArrayListTest.java

--- test/ClassProject/Database/ArrayListTest.java
+++ test/ClassProject/Database/ArrayListTest.java
@@ -6,22 +6,25 @@ import java.util.*;
 
 public class ArrayListTest extends TestCase {
 	public void testCreate() {
-		ArrayListDatabase<String> db = new ArrayListDatabase<String>(5);
-	 	db.add("Arpan");
-	 	db.add("Alexa");
-	 	db.add("Nick");
+		ArrayListDatabase db = new ArrayListDatabase(5);
+	 	db.addStudent(1000);
+	 	db.addStudent(2000);
+	 	db.addStudent(3000);
 	 	System.out.println();
 	 	
-	 	db.remove(2); 	
-	 	db.set(1, "Bryan");
-	 	//change "Bryan" if you want assertEquals statement to fail
-	 	assertEquals("Bryan", db.getStudent(1));
+		db.trimSize();
+		assertEquals(db.returnSize(), 3);
+
+	 	db.removeStudent(2); 	
+	 	db.setStudent(1, 5000);
+	 	//change "5000" if you want assertEquals statement to fail
+	 	assertEquals((int)db.getStudent(1), 5000);
 	 	System.out.println();
 	 	
-	 	System.out.print("Number of students:");
+	 	System.out.print("Number of students: ");
 	 	System.out.println(db.returnSize());	
 
-	 	db.clearAll();
+	 	db.clearAllStudents();
 	 	assertEquals(db.returnSize(), 0);
 	}
 }
