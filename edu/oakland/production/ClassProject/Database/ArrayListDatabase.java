01d0d97cc374106f5e79a65b4903eea39dc1bad5
auljaj <auljaj@users.noreply.github.com>
2015-04-09 00:49:26 -0400

    Update ArrayListDatabase.java

--- edu/oakland/production/ClassProject/Database/ArrayListDatabase.java
+++ edu/oakland/production/ClassProject/Database/ArrayListDatabase.java
@@ -11,53 +11,69 @@ import java.util.*;
 *@since "152503"
 */
 public class ArrayListDatabase{
-	
-		/** Declare arraylist 
+	/**
+	* Declares an Integer arraylist named students.
 	*/
 	public ArrayList<Integer> students;
-	
-	/**Overloaded constructor to create ArrayList object of size "size"
+	/**
+	* Overloaded constructor used to create an ArrayList object of size "size"
+	* 
+	* @param size Specified size of the array.
 	*/
 	public ArrayListDatabase(int size){
 		students = new ArrayList<Integer>(size);
 	}
-	
-	/**Get Student Method
-	* @param index of student from array
-	* @return student from selected index
+	/**
+	* Returns the element at the specified position in this list.  
+	* 
+	* @param i The index of the element to return.
+	* @return Returns the element at the specified position in this list .
 	*/
 	public Integer getStudent(int i){
 		return students.get(i);
 	}
-
-	/**set Student Method
-	* @param index of student from array
+	/**
+	* Adds a student onto the arraylist at the specified position.
+	* 
+	* @param i This is the index of the element to replace.
+	* @param j This is the element to be stored at the specified position.
 	*/
 	 public void setStudent(int i, int j){
 	 	 students.set(i,j);
 	 }
-	 
-	/**Add student method
+	/**
+	* Appends the specified element to the end of this list.
+	* 
+	* @param i This is the element to be appended to the list.
 	*/
 	 public  void addStudent(int i){
 	 	 students.add(i);
 	 } 
-
-	/**remove Student Method
-	* @param index of student from array
+	/**
+	* Removes the element at the specified position in this list.
+	* 
+	* @param i The index of the element to be removed.
 	*/
 	 public void removeStudent(int i){
 	 	 students.remove(i);
 	 }
-	 
+	 /**
+	  * Returns the number of elements in this list. 
+	  * 
+	  * @return The number of elements in this list.
+	  */
 	 public int returnSize(){
 	 	 return students.size();
 	 }
-	 
+	 /**
+	  * Removes all of the elements in this list.
+	  */	 
 	 public void clearAllStudents(){
 	 	 students.clear();
 	 }
-	 
+	 /**
+	  * Trims the capacity of this ArrayList instance to be the list's current size..
+	  */
 	 public void trimSize(){
 	 	 students.trimToSize();
 	 }
