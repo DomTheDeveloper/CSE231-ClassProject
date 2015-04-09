0a5b6467eb8b3669a045af9258ea6dda0f844e0e
Jacques Breaux <BreauxJaqJr@comcast.net>
2015-04-08 21:34:13 -0400

    Modified Test to work properly

--- edu/oakland/test/ClassProject/Database/TestLinkedListDB.java
+++ edu/oakland/test/ClassProject/Database/TestLinkedListDB.java
@@ -5,7 +5,7 @@ import junit.framework.*;
 
 public class TestLinkedListDB extends TestCase {
 	public void testCreate() {
-		LinkedListDB list = new LinkedListDB();
+		LinkedListDB<Integer> list = new LinkedListDB<Integer>();
 		
         // add elements to LinkedList
         list.add(10);
@@ -14,17 +14,17 @@ public class TestLinkedListDB extends TestCase {
         list.add(44);
         list.add(50);
  
-        //checks if 5th element = 50 - then it was added properly
-        assertEquals(list.get(5), 50);
+        //checks if 5th element = 10 - then it was added properly
+        assertEquals(list.get(4).getData().intValue(), 10);
         
         System.out.println("LinkedList size: " + list.size());
-        System.out.println("Get 3rd element: " + list.get(3));
-        System.out.println("Remove 4nd element: " + list.remove(4));
+        System.out.println("Get 3rd element: " + list.get(2));
+        System.out.println("Remove First element: " + list.removeFirst());
         System.out.println("LinkedList size: " + list.size());
         
-        list.set(2, 100);
+        list.set(1, 100);
         //checks if 2nd element = 100 - then it was set properly
-        assertEquals(list.get(2), 100);
+        assertEquals(list.get(1).getData().intValue(), 100);
         
         System.out.println("LinkedList size: " + list.size());
     }
