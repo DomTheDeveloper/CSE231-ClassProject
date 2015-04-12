00da926b4d0eafbc6ebcf85c24c81ab205c440ee
auljaj <auljaj@users.noreply.github.com>
2015-04-12 14:40:47 -0400

    Update ArrayListTest.java
    Tests all methods separately.

--- source/edu/oakland/test/ClassProject/Database/ArrayListTest.java
+++ source/edu/oakland/test/ClassProject/Database/ArrayListTest.java
@@ -5,26 +5,50 @@ import junit.framework.*;
 import java.util.*;
 
 public class ArrayListTest extends TestCase {
-	public void testCreate() {
-		ArrayListDatabase db = new ArrayListDatabase(5);
+	
+	ArrayListDatabase db;
+	
+	public void testGetStudent(){
+		initTest();
+		assertEquals((int)db.getStudent(2), 3000);
+	}
+	
+	public void testSetStudent(){
+		initTest();
+		db.setStudent(1, 5000);
+	 	assertEquals((int)db.getStudent(1), 5000);
+	}
+	
+	public void testRemoveStudent(){
+		initTest();
+		db.removeStudent(0);
+		assertEquals((int)db.getStudent(0), 2000);
+	}
+	
+	public void testReturnSize(){
+		initTest();
+		assertEquals(db.returnSize(), 3);
+	}
+	
+	public void testTrimSize(){
+		initTest();
+		db.trimSize();
+		assertEquals(db.returnSize(), 3);
+	}
+	
+	public void testClearAllStudents(){
+		initTest();
+		db.clearAllStudents();
+		assertEquals(db.returnSize(), 0);
+	}
+	
+	/**
+	* .addStudent() tested within initTest() - no separate test needed
+	*/
+	public void initTest(){
+		db = new ArrayListDatabase(5);
 	 	db.addStudent(1000);
 	 	db.addStudent(2000);
 	 	db.addStudent(3000);
-	 	System.out.println();
-	 	
-		db.trimSize();
-		assertEquals(db.returnSize(), 3);
-
-	 	db.removeStudent(2); 	
-	 	db.setStudent(1, 5000);
-	 	//change "5000" if you want assertEquals statement to fail
-	 	assertEquals((int)db.getStudent(1), 5000);
-	 	System.out.println();
-	 	
-	 	System.out.print("Number of students: ");
-	 	System.out.println(db.returnSize());	
-
-	 	db.clearAllStudents();
-	 	assertEquals(db.returnSize(), 0);
 	}
 }
