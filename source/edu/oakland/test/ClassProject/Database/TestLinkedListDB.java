77fdd8d456c82a5829a8c49e81c279e6ec027a54
auljaj <auljaj@users.noreply.github.com>
2015-04-12 14:19:50 -0400

    Update TestLinkedListDB.java
    Now tests each method separately.

--- source/edu/oakland/test/ClassProject/Database/TestLinkedListDB.java
+++ source/edu/oakland/test/ClassProject/Database/TestLinkedListDB.java
@@ -2,30 +2,63 @@ package edu.oakland.test.ClassProject.Database;
 
 import edu.oakland.production.ClassProject.Database.*;
 import junit.framework.*;
-
+/**
+* .get() method was tested in many of the assertEquals statements.
+*/
 public class TestLinkedListDB extends TestCase {
-	public void testCreate() {
-		LinkedListDB<Integer> list = new LinkedListDB<Integer>();
-		
-        // add elements to LinkedList
-        list.add(10);
-        list.add(22);
-        list.add(30);
-        list.add(44);
-        list.add(50);
- 
-        //checks if 5th element = 10 - then it was added properly
-        assertEquals(list.get(4).getData().intValue(), 10);
-        
-        System.out.println("LinkedList size: " + list.size());
-        System.out.println("Get 3rd element: " + list.get(2));
-        System.out.println("Remove First element: " + list.removeFirst());
-        System.out.println("LinkedList size: " + list.size());
-        
-        list.set(1, 100);
-        //checks if 2nd element = 100 - then it was set properly
-        assertEquals(list.get(1).getData().intValue(), 100);
-        
-        System.out.println("LinkedList size: " + list.size());
-    }
+	
+	LinkedListDB<Integer> list = new LinkedListDB<Integer>();
+    
+	public void testSize(){
+		list.add(10);
+    		list.add(22);
+    		list.add(30);
+    		assertEquals(list.size(), 3);
+	}
+	
+	public void testIsEmpty(){
+		assertEquals(list.isEmpty(), true);
+	}
+	
+    	public void testAdd(){
+    		list.add(10);
+    		assertEquals(list.get(0).getData().intValue(), 10);
+    	}
+    	
+    	public void testInsertFirstLink(){
+    		list.insertFirstLink(20);
+    		assertEquals(list.get(0).getData().intValue(), 20);
+    	}
+    	
+    	public void testRemoveFirst(){
+    		list.add(100);
+    		list.add(98);
+    		assertEquals(list.removeFirst().intValue(), 98);
+    	}
+    	
+    	public void testFind(){
+    		list.add(10);
+    		list.add(22);
+    		list.add(30);
+    		list.add(44);
+    		list.add(50);
+    		assertEquals(list.find(30).getData().intValue(), 30);
+    	}
+    	
+    	public void testRemoveLink(){
+    		list.add(10);
+    		list.add(22);
+    		list.add(30);
+    		list.add(44);
+    		list.add(50);
+    		assertEquals(list.removeLink(44).getData().intValue(), 44);
+    	}
+    	
+    	public void testSet(){
+    		list.add(10);
+    		list.add(22);
+    		list.add(30);
+    		list.set(1, 100);
+    		assertEquals(list.get(1).getData().intValue(), 100);
+    	}
 }
